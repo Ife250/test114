6c9d9f00077ad2aeefff9f4c51ea9205b1e7d0d4
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2022-03-07 06:29:30 -0500

    Update tokio.js

--- plugins/tokio.js
+++ plugins/tokio.js
@@ -1,5 +1,5 @@
 let handler = async m => m.reply(`
-☕ *Tokio :* a whatsapp bot\n\n🔗 *URL :* https://github.com/itsajaygaur/tokio-wabot
+☕ *Tokio :* a whatsapp bot\n\n🔗 *URL :* https://github.com/dark-worf/test114
 `.trim()) // repository
 handler.help = ['tokio']
 handler.tags = ['info']
