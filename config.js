5f95c6d57f657c341f08a4728bb50978631e20c4
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-04-08 16:05:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['265994894966'] // Put your number here
+global.owner = ['265889976968'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
